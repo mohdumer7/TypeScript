b2e3775be521805d9660238267f4ba152a7125e0
Mohammed Umer <52200381+mohdumer7@users.noreply.github.com>
2023-04-19 00:24:02 +0530

    Added abstract classes

--- classes/app.ts
+++ classes/app.ts
@@ -31,7 +31,7 @@ console.log(hostCopy.describe());
 //----------------------------------------------------------------------------
 //////////////////////////////////////////////////////////////////////////////////////
 
-//private and public class methods
+//private ,protected and public  class methods
 
 class Department1 {
   // name: string;
@@ -42,6 +42,10 @@ class Department1 {
   //readonly allows you to never change the value even inside the class
   //once set through a constructor its donee
   private readonly id: string;
+
+  //Protected method keeps the value private but allows other class to access it
+  //the key difference is it dosent allow it to be accessed globally
+  protected salary: number = 0;
 
   // constructor(n: string) {
   //   this.name = n;
@@ -54,6 +58,10 @@ class Department1 {
   }
   //----------------------------------------------------------------------------
 
+  //if abstract is added make sure to add abstract label to the class definition
+  //abstact class Department1 <---this must be written on Line 36
+  // abstract describe(this: Department) :void
+
   describe(this: Department) {
     console.log("My dept is" + this.name);
   }
@@ -64,6 +72,7 @@ class Department1 {
     //becaue id is set to readonly i cannot changeits value
     // this.id = '3'
     this.employees.push(employee);
+    this.salary = 1000;
   }
 
   printEmployeeInformation() {
@@ -85,10 +94,86 @@ department1.name = "Mechanical";
 //INHERITENCE
 //can only inherit from one class
 class ITDepartment extends Department1 {
+  //----------------------------------------------------------------------------
+  private lastReport: string;
+
+  //getter and setter
+  get mostRecentReport() {
+    return "most recent report" + this.lastReport;
+  }
+  set mostRecentReport(val: string) {
+    this.lastReport = val;
+  }
+  //----------------------------------------------------------------------------
+  //you can use any method as STATIC and this can used anywhere without
+  //instantiating the whole class
+  static fiscalYear = 2020;
+  static craeteEmployee(name: string) {
+    return { name: name };
+  }
+  //----------------------------------------------------------------------------
   //inherit from the parent class calls super
-  constructor(public name: string) {
+  constructor(public name: string, public yearsOld: number) {
+    //name is sent to the parent class
+    //years old i a new class only for its own use case of the class
     super(name);
+    this.lastReport = yearsOld.toString();
+    //THIS IS NOT POSSIBLE FOR STATIC VALUES
+    //because this is refrenced to this but static is detached to this
+    //                   console.log(this.fiscalYear);
+    //You can only use it like this
+    console.log(ITDepartment.fiscalYear);
+  }
+  //----------------------------------------------------------------------------
+
+  //method overriding
+  addEmployee(employee: string): void {
+    if (this.name === "umer") {
+      // The salary is acccesible because it is protected in the Department class
+      console.log(this.salary);
+      // prints:0
+      return;
+    }
   }
 }
+
+const accounting = new ITDepartment("DCT", 8);
+
+//this can access all the methods of its parents class
+console.log(accounting.name);
+//You dont have to call it like a function the getter works like a variable
+console.log(accounting.mostRecentReport);
+accounting.mostRecentReport = "99";
+//STATIC method usage
+//NoTICE that there is no new keywword used here as it is a static method
+const employee1 = ITDepartment.craeteEmployee("umer");
+console.log(employee1, ITDepartment.fiscalYear);
+//----------------------------------------------------------------------------
+
+//////////////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------------------------
+//////////////////////////////////////////////////////////////////////////////////////
+
+//ABSTRACT CLASSES
+//helps in making BLUEPRINT methods in the Parent classes to be overwritten
+class ITDepartment1 extends Department1 {
+  //----------------------------------------------------------------------------
+  constructor(public name: string, public yearsOld: number) {
+    super(name);
+  }
+  //----------------------------------------------------------------------------
+  //this describe method is also available on the Department1 which mean
+  //METHOD OVERIDING
+  //but sometimes we need to force them tooverride the method
+  //then the same descibe class in the parent must be lablled abstact
+  //such that anybody using our class anywhere has to at any cost overrirde
+  //and must and should use it.
+  //LINE 61 TO 64 in Department1 class is shown
+  describe() {
+    console.log("Accounting Department", this.name);
+  }
+}
+
+const accounting1 = new ITDepartment("DCT", 8);
 
 //----------------------------------------------------------------------------
