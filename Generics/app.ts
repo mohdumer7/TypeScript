638793bc5359b84e0d3c9ac76f5cfe8553d8174c
Mohammed Umer <52200381+mohdumer7@users.noreply.github.com>
2023-04-19 10:09:55 +0530

    Added all generic types

--- Generics/app.ts
+++ Generics/app.ts
@@ -60,3 +60,31 @@ function extractandConvert<T extends Object, U extends keyof T>(
 }
 
 extractandConvert({ name: "umer" }, "name");
+
+//------------------------------------------------------------------------------
+// GENERIC CLASSES
+
+class DataStorage<T> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+const numberStorage = new DataStorage<number>();
+
+textStorage.addItem("shoes");
+//cant do this as only strings can be stored not numbers in thsi class
+// textStorage.addItem(1);
+numberStorage.addItem(1);
+//------------------------------------------------------------------------------
